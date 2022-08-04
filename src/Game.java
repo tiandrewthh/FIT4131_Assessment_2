231d49615710edc9d5b2cc34b5c088fdf9af2ba3
Andrew Tran <tiandrewthh@gmail.com>
2022-08-04 14:55:25 +1000

    Changes to Game and Menu class

--- src/Game.java
+++ src/Game.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 public class Game {
     private Hunter hunter;
     private String mode;
+    private Menu menu;
+    private int turns;
+    private int turnLimit;
+    private int targetFishBal;
+    private int dailyFish;
     final static private String ARCADE = "Arcade";
     final static private String STORY = "Story";
     final static private String WEAPONS_TEXTFILE = "weapons.txt";
     public Game() {
         mode = ARCADE;
         hunter = new Hunter();
+        menu = new Menu();
+
     }
 
     public Game(String mode, Hunter hunter) {
         this.mode = mode;
         this.hunter = hunter;
     }
+
+
 
     public String display() {
         return "Game{" +
@@ -23,7 +32,11 @@ public class Game {
                 '}';
     }
 
-
+    public void inputHunterName(String name) {
+        Input input = new Input();
+        String hunterName = input.acceptStringInput("");
+        getHunter().setName(name);
+    }
     public Hunter getHunter() {
         return hunter;
     }
@@ -32,24 +45,45 @@ public class Game {
         return mode;
     }
 
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public int getTurnLimit() {
+        return turnLimit;
+    }
+
+    public int getTargetFishBal() {
+        return targetFishBal;
+    }
+
+    public int getDailyFish() {
+        return dailyFish;
+    }
+
     public static void main(String[] args) {
         startGame();
     }
 
-    public void readFile() {
-        FileIO fileIO = new FileIO(WEAPONS_TEXTFILE);
+    public void readFile(String fileName) {
+        FileIO fileIO = new FileIO(fileName);
         String[] fileContents = fileIO.readFile().split(System.lineSeparator());
         int counter = 0;
 
         for (String lineContent : fileContents) {
             String[] lineValues = lineContent.split(",");
             String weaponName = lineValues[0];
-            String weaponDmg = lineValues[1];
+            int weaponDmg = Integer.parseInt(lineValues[1]);
             String weaponStrong = lineValues[2];
             String weaponWeak = lineValues[3];
-            String weaponCost = lineValues[4];
-            String weaponMin = lineValues[5];
-            String weaponMax = lineValues[6];
+            int weaponCost = Integer.parseInt(lineValues[4]);
+            int weaponMin = Integer.parseInt(lineValues[5]);
+            int weaponMax = Integer.parseInt(lineValues[6]);
+            getHunter().addWeapon(new Weapon(weaponCost, weaponDmg, weaponMin, weaponMax, weaponName, weaponStrong, weaponWeak));
         }
     }
 
@@ -57,7 +91,32 @@ public class Game {
         this.mode = mode;
     }
 
+    public void setHunter(Hunter hunter) {
+        this.hunter = hunter;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+
+    public void setTurnLimit(int turnLimit) {
+        this.turnLimit = turnLimit;
+    }
+
+    public void setTargetFishBal(int targetFishBal) {
+        this.targetFishBal = targetFishBal;
+    }
+
+    public void setDailyFish(int dailyFish) {
+        this.dailyFish = dailyFish;
+    }
+
     public static void startGame() {
         Game newGame = new Game();
+        newGame.readFile(WEAPONS_TEXTFILE);
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,31 @@
 public class Menu {
+    public Menu() {
 
+    }
+
+    public String getStartMenu() {
+        String startMenu = ("  _____                       _         _    _             _            \n" +
+                " |  __ \\                     (_)       | |  | |           | |           \n" +
+                " | |__) |__ _ __   __ _ _   _ _ _ __   | |__| |_   _ _ __ | |_ ___ _ __ \n" +
+                " |  ___/ _ \\ '_ \\ / _` | | | | | '_ \\  |  __  | | | | '_ \\| __/ _ \\ '__|\n" +
+                " | |  |  __/ | | | (_| | |_| | | | | | | |  | | |_| | | | | ||  __/ |   \n" +
+                " |_|   \\___|_| |_|\\__, |\\__,_|_|_| |_| |_|  |_|\\__,_|_| |_|\\__\\___|_|   \n" +
+                "                   __/ |                                                \n" +
+                "                  |___/                                                 " + "\n" +
+                "Please enter a name between 3 and 12 characters:"
+        );
+        return startMenu;
+    }
+
+//    public String getPlayMenu() {
+//        String playMenu =
+//        return playMenu;
+//    }
+//
+//    public String getGameModeSelectionMenu
+//
+//    public String getStoryModeMenu() {
+//        String StoryModeMenu = "";
+//        return StoryModeMenu;
+//    }
 }
